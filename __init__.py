03c2c6ddbcaaca7ee17f5a62892cb863e50eb90f
eliesat <112230228+eliesat@users.noreply.github.com>
2025-10-18 11:44:42 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-Version = "3.56"
+Version = "3.57"
 Panel = 'ElieSatPanel'
--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@ clear >/dev/null 2>&1
 #configuration
 ###########################################
 plugin=beta-main
-version='3.56'
+version='3.57'
 changelog='1.18.10.2025'
 url=https://github.com/eliesat/beta/archive/main.tar.gz
 package=/tmp/$plugin.tar.gz
